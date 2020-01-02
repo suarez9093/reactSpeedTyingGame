7cf4daa822c8c7769a7267770a7eb05d70fa596e
Alex <suarez9093@hotmail.com>
2020-01-01 20:14:39 -0700

    added start game and end game functions as well as disabled buttons and text area on certain events throughout the game

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React, { Fragment, useState, useEffect } from "react";
 
-/**
- * Challenge:
- * 
- * Make it so clicking the Start button starts the timer instead of it starting on refresh
- * (Hint: use a new state variable to indicate if the game should be running or not)
- */
-
 
 function App() {
+  const STARTING_TIME = 5
 
   const [text, setText] = useState();
-  const [timeRemaining, setTimeRemaining] = useState(5);
-  const [ isTimeRunning , setIsTimeRunning ] = useState(false);
+  const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME);
+  const [isTimeRunning, setIsTimeRunning] = useState(false);
+  const [wordCount, setWordCount] = useState(0);
 
   function handleChange(e) {
     const { value } = e.target;
@@ -23,33 +18,43 @@ function App() {
   function calculateWordCount(text) {
     const wordsArr = text.trim().split(" ");
     const filteredWords = wordsArr.filter(word => word !== "")
-    return filteredWords.length
+    setWordCount(filteredWords.length)
   }
 
+  function startGame() {
+    setTimeRemaining(STARTING_TIME);
+    setIsTimeRunning(true)
+    setWordCount(0)
+    setText("")
+  }
 
+  function endGame() {
+    setIsTimeRunning(false)
+    calculateWordCount(text)
+  }
 
-      useEffect(() => {
-        if(isTimeRunning && timeRemaining !== 0){
-          setTimeout(() => {
-            setTimeRemaining(time => time - 1);
-          }, 1000)
-        } else if(timeRemaining === 0 ){
-          setIsTimeRunning(false)
-          console.log("isTimeRunning: ",isTimeRunning)
-        }
-      }, [timeRemaining,isTimeRunning])
+  useEffect(() => {
+    if (isTimeRunning && timeRemaining !== 0) {
+      setTimeout(() => {
+        setTimeRemaining(time => time - 1);
+      }, 1000)
+    } else if (timeRemaining === 0) {
+      endGame()
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [timeRemaining, isTimeRunning])
 
   return (
     <Fragment>
       <h1>Speed Typing Game</h1>
       <textarea
+        disabled={!isTimeRunning}
         value={text}
         onChange={handleChange}
       />
       <h4>Time remaining: {timeRemaining} seconds </h4>
-      <button onClick={() => setIsTimeRunning(true)}>Start Game</button>
-      {/* <button onClick={() => console.log(calculateWordCount(text))}>calculate words</button> */}
-      <h1>Word Count: </h1>
+      <button disabled={isTimeRunning} onClick={startGame}>Start Game</button>
+      <h1>Word Count: {wordCount}</h1>
     </Fragment>
   );
 }
