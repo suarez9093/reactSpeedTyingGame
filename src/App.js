ad8ea0911a869dfccfa3988db926b8d0f0e62222
Alex <suarez9093@hotmail.com>
2020-01-01 18:52:22 -0700

    added functional timer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 
 /**
  * Challenge:
  * 
- * Create a function to calculate the number of separate words in the `text` state
- * For now, just console.log the word count when the button gets clicked to test it out.
+ * 1. Create state to hold the current value of the countdown timer.
+ *    Display this time in the "Time Remaining" header
  */
 
 function App() {
 
   const [text, setText] = useState();
-  const [words, setWords] = useState();
+  const [timer, setTimer] = useState(5);
 
   function handleChange(e) {
     const { value } = e.target;
@@ -24,11 +24,15 @@ function App() {
     return filteredWords.length
   }
 
-  // console.log("calculateWords: ",calculateWords())
 
-  console.log("words: ", words)
+  useEffect(() => {
+    if(timer !== 0){
+      setTimeout(() => {
+        setTimer(timeRemaining => timeRemaining - 1);
+      }, 1000)
+    }
+  }, [timer])
 
-  console.log(text)
   return (
     <Fragment>
       <h1>Speed Typing Game</h1>
@@ -36,7 +40,7 @@ function App() {
         value={text}
         onChange={handleChange}
       />
-      <h4>Time remaining: </h4>
+      <h4>Time remaining: {timer} seconds </h4>
       <button >Start Game</button>
       <button onClick={() => console.log(calculateWordCount(text))}>calculate words</button>
       <h1>Word Count: </h1>
