f55f86c5b1c09120b30b9a6aae2dded0723b1929
Alex <suarez9093@hotmail.com>
2020-01-01 18:29:30 -0700

    added function to count words

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState} from "react";
+import React, { Fragment, useState } from "react";
 
 /**
  * Challenge:
@@ -6,37 +6,39 @@ import React, { Fragment, useState} from "react";
  * Create a function to calculate the number of separate words in the `text` state
  * For now, just console.log the word count when the button gets clicked to test it out.
  */
- 
+
 function App() {
 
-  const [ text, setText ] = useState();
-  const [ words , setWords ] = useState();
+  const [text, setText] = useState();
+  const [words, setWords] = useState();
 
-  function handleChange(e){
-    const {value} = e.target;
+  function handleChange(e) {
+    const { value } = e.target;
 
     setText(value)
   }
 
-  function calculateWords(text){
-    const wordsInTextArea = text.split(" ").length
-
-    setWords(wordsInTextArea);
+  function calculateWordCount(text) {
+    const wordsArr = text.trim().split(" ");
+    const filteredWords = wordsArr.filter(word => word !== "")
+    return filteredWords.length
   }
+
+  // console.log("calculateWords: ",calculateWords())
 
   console.log("words: ", words)
 
-console.log(text)
+  console.log(text)
   return (
     <Fragment>
       <h1>Speed Typing Game</h1>
       <textarea
-      value={text}
-      onChange={handleChange} 
+        value={text}
+        onChange={handleChange}
       />
       <h4>Time remaining: </h4>
       <button >Start Game</button>
-      <button onClick={calculateWords}>calculate words</button>
+      <button onClick={() => console.log(calculateWordCount(text))}>calculate words</button>
       <h1>Word Count: </h1>
     </Fragment>
   );
